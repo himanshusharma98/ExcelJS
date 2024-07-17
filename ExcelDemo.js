adc09e6dac6d9893f136d2dfc80b81452f96237f
sharma <sharma@DESKTOP-IS2799H>
2024-07-17 18:45:45 +0530

    Reading excel data

--- ExcelDemo.js
+++ ExcelDemo.js
@@ -1,7 +1,13 @@
-import {ExcelJS} from 'exceljs'
-
+const ExcelJS = require('exceljs');
+async function  ExcelDemo() {
+    
 const workbook = new ExcelJS.Workbook();
+await workbook.xlsx.readFile('C:\Users\sharma\Downloads\download.xlsx');
 const worksheet = workbook.getWorksheet('Sheet1');
 worksheet.eachRow((row, rowNumber) => {
-    
+    row.eachCell((cell, colNumber) => {
+        console.log(cell.value);
+    })
 })
+}
+ExcelDemo();
