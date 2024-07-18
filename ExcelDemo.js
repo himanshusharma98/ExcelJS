36071b818e3ca123e9529c6302261a7d5148c2d7
sharma <sharma@DESKTOP-IS2799H>
2024-07-18 18:29:16 +0530

    New Changes

--- ExcelDemo.js
+++ ExcelDemo.js
@@ -1,13 +1,28 @@
 const ExcelJS = require('exceljs');
-async function  ExcelDemo() {
-    
-const workbook = new ExcelJS.Workbook();
-await workbook.xlsx.readFile('C:\Users\sharma\Downloads\download.xlsx');
-const worksheet = workbook.getWorksheet('Sheet1');
-worksheet.eachRow((row, rowNumber) => {
-    row.eachCell((cell, colNumber) => {
-        console.log(cell.value);
-    })
-})
+
+async function ExcelDemo() {
+    try {
+        let output = {row:-1,column:-1};
+        const workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.readFile('D:\\ExcelJS\\ExcelFile.xlsx');
+        const worksheet = workbook.getWorksheet('Sheet1');
+        worksheet.eachRow((row, rowNumber) => {
+            row.eachCell((cell, colNumber) => {
+                console.log(cell.value);
+                if(cell.value === "Banana") {
+                    output.row = rowNumber;
+                    output.column = colNumber;
+            
+                }
+            })
+        })
+
+        const cell = worksheet.getCell(output.row, output.column); 
+        cell.value = "Republic";  
+        await workbook.xlsx.writeFile('D:\\ExcelJS\\ExcelFile.xlsx');
+    } catch (error) {
+        console.error(error);
+    }
 }
+
 ExcelDemo();
