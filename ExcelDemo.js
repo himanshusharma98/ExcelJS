2ca908f9fb0c6ff5fe0d446844db22dee283032c
sharma <sharma@DESKTOP-IS2799H>
2024-07-19 18:19:22 +0530

    New Change in ExcelJS

--- ExcelDemo.js
+++ ExcelDemo.js
@@ -1,28 +1,35 @@
-const ExcelJS = require('exceljs');
+const ExcelJs = require('exceljs');
+import { test } from '@playwright/test';
 
-async function ExcelDemo() {
-    try {
-        let output = {row:-1,column:-1};
-        const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.readFile('D:\\ExcelJS\\ExcelFile.xlsx');
-        const worksheet = workbook.getWorksheet('Sheet1');
-        worksheet.eachRow((row, rowNumber) => {
-            row.eachCell((cell, colNumber) => {
-                console.log(cell.value);
-                if(cell.value === "Banana") {
-                    output.row = rowNumber;
-                    output.column = colNumber;
-            
-                }
-            })
-        })
 
-        const cell = worksheet.getCell(output.row, output.column); 
-        cell.value = "Republic";  
-        await workbook.xlsx.writeFile('D:\\ExcelJS\\ExcelFile.xlsx');
-    } catch (error) {
-        console.error(error);
-    }
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet('Sheet1');
+  const output = await readExcel(worksheet, searchText);
+
+  const cell = worksheet.getCell(output.row, output.column + change.colChange);
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+
 }
 
-ExcelDemo();
+
+async function readExcel(worksheet, searchText) {
+  let output = { row: -1, column: -1 };
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
+
+
+    })
+
+  })
+  return output;
+}
+
+
